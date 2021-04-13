00aed37f23cad5c4f142d722ea2c719641be1267
TimWD <timgerm280@gmail.com>
2021-04-13 16:54:09 +0500

    finised thank you section

--- static/js/mentor_index.js
+++ static/js/mentor_index.js
@@ -12,20 +12,20 @@ let mentorsArray = () => {
             for (mentor of data) {
                mentorStuff = `
                <div class="card">
-                <div class="flex fd-row jc-fs">
-                    <div class="mentor_image">
-                        <img src="${mentor.image}" alt="mentor_photo">
-                    </div>
-                    <div class="name_link">
-                        <h3>${mentor.mentor_name}</h3>
-                        <div class="flex fd-row jc-fs">
-                            <a href="${mentor.twitter_link}" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
-                            <a href="${mentor.youtube_link}" target="_blank"><i class="fab fa-youtube fa-2x"></i></a>
-                            <a href="" class="button">View</a>
+                    <div class="flex fd-row jc-fs">
+                        <div class="mentor_image">
+                            <img src="/static/${mentor.image}" alt="mentor_photo">
+                        </div>
+                        <div class="name_link">
+                            <h3>${mentor.mentor_name}</h3>
+                            <div class="flex fd-row jc-fs">
+                                <a href="${mentor.twitter_link}" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+                                <a href="${mentor.youtube_link}" target="_blank"><i class="fab fa-youtube fa-2x"></i></a>
+                                <a href="" class="button">View</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             ` 
             all_mentor += mentorStuff
             }
