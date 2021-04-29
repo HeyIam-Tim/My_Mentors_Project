138a1b376838e991706e420d71e1446184eadfc2
TimWD <timgerm280@gmail.com>
2021-04-29 11:31:04 +0500

    added favicon

--- my_mentors/urls.py
+++ my_mentors/urls.py
@@ -24,7 +24,7 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mentors.urls')),
-    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon1.ico")),),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("myFavicon.ico")),),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
